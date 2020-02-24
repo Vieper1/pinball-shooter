9e669863aa57eb6e6b2312982efbc40074765119
naiduvishal13 <naiduvishal13@gmail.com>
2020-02-23 17:07:25 -0700

    Made a respawn trigger box

--- Assets/Scripts/GameConfig.cs
+++ Assets/Scripts/GameConfig.cs
@@ -7,16 +7,19 @@ public class GameConfig : MonoBehaviour
 	public static float BULLET_LIFETIME = 1.0f;
 	public static float BULLET_FORCE_MULTIPLIER = 1.0f;
 	public static float SLOWMOTION_FACTOR = 0.05f;
+	public static float RESPAWN_DELAY = 1.0f;
 
 	// Setters
 	public float Bullet_LifeTime = 1.0f;
 	public float Bullet_ForceMultiplier = 1.0f;
 	public float Slowmotion_Factor = 1.0f;
+	public float Respawn_Delay = 1.0f;
 
 	void Start()
 	{
 		BULLET_LIFETIME = Bullet_LifeTime;
 		BULLET_FORCE_MULTIPLIER = Bullet_ForceMultiplier;
 		SLOWMOTION_FACTOR = Slowmotion_Factor;
+		RESPAWN_DELAY = Respawn_Delay;
 	}
 }
---
+++ Assets/Scripts/ResetBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBox : MonoBehaviour
+{
+	[Header("Config")]
+	public GameObject Ball;
+
+	private Vector3 resetPosition;
+	private Rigidbody2D ballRb2d;
+
+    void Start()
+    {
+		resetPosition = Ball.transform.position;
+		ballRb2d = Ball.GetComponent<Rigidbody2D>();
+    }
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.transform.CompareTag("Ball"))
+		{
+			StartCoroutine(RespawnBallInSeconds());
+		}
+	}
+
+	IEnumerator RespawnBallInSeconds()
+	{
+		Ball.transform.position = new Vector3(9999f, 9999f);
+		yield return new WaitForSeconds(GameConfig.RESPAWN_DELAY);
+		Ball.transform.position = resetPosition;
+		ballRb2d.velocity = Vector3.zero;
+	}
+}
